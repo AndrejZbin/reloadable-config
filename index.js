377175c3d928c2c86110fb7cd93e73e3beaf2ee7
Andrej Zbin <zbin.andrej@gmail.com>
2019-11-13 11:02:14 +0100

    az: initial commit

---
+++ index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const crypto = require('crypto');
+
+class Config {
+    constructor() {
+        // object created at app load and never changed
+        this._config = {
+            file: undefined,
+            autoreload: 0,
+            on_reload_begin: () => {},
+            on_reload_end: () => {},
+            defaults: {},
+        };
+        // object created at first reload and
+        this._staticConfig = {};
+        // object updated every time reload is called
+        this._dynamicConfig = {};
+        // callbacks called when reload is called with old config and new config as params
+        this._callbacks = {};
+        // config was at least once loaded
+        this._initialized = false;
+        // interval
+        this._autoreload = null;
+    }
+    setup(config, reload=true) {
+        // override default settings with specified settings
+        Object.assign(this._config, config);
+        // first we copy passed object so it's not modified
+        let defaults_static = Object.assign({}, this._config.defaults);
+        let defaults_dynamic = Object.assign({}, this._config.defaults);
+        // then we assign current values to defaults, so we keep values we already have and add defaults for values we dont have
+        // the result is already a new config, but we can't replace it with =, so we use assign again
+        Object.assign(this._staticConfig, Object.assign(defaults_static, this._staticConfig));
+        Object.assign(this._dynamicConfig, Object.assign(defaults_dynamic, this._dynamicConfig));
+        if (config.hasOwnProperty('autoreload')) {
+            if (this._autoreload != null) clearInterval(this._autoreload);
+            if (config.autoreload) this._autoreload = setInterval(() => this.reload(), config.autoreload);
+        }
+        if (reload) this.reload();
+        return this;
+    }
+    reload() {
+        this._config.on_reload_begin();
+        // verify file exists
+        try {
+            require.resolve(this._config.file);
+        }
+        catch(e) {
+            console.error(`Error while loading config file \`${this._config.file}\`.`);
+            // throw error only if file was never loaded
+            if (!this._initialized) throw Error('Cannot load config file')
+        }
+        const old_config = Object.assign({}, this._dynamicConfig);
+        delete require.cache[this._config.file];
+        let new_config = require(this._config.file);
+
+        for (const prop of Object.keys(this._dynamicConfig)) {
+            delete this._dynamicConfig[prop];
+        }
+        // first load, set the static config
+        if (!this._initialized) {
+            this._initialized = true;
+            Object.assign(this._staticConfig, new_config);
+        }
+        Object.assign(this._dynamicConfig, new_config);
+
+        for (let callback of Object.values(this._callbacks)) {
+            if (callback.watch === '*') {
+                callback.fun(new_config, old_config);
+            }
+            else {
+                for (let p of callback.watch) {
+                    if (old_config[p] + '' !== new_config[p] + '')  {
+                        callback.fun(new_config, old_config);
+                        break;
+                    }
+                }
+            }
+        }
+        this._config.on_reload_end();
+    }
+    subscribe(callback) {
+        // only function with default config
+        if (typeof callback !== 'object') {
+            callback = {
+                handler: callback,
+            }
+        }
+        // assign default values
+        let _options = Object.assign({
+            id: crypto.randomBytes(12).toString('hex'),
+            init_call: true,
+            watch: '*',
+        }, callback);
+        if (!callback.handler instanceof Function) {
+            throw new Error('Callback is not a function');
+        }
+        this._callbacks[_options.id] = {
+            fun: callback.handler,
+            watch: _options.watch,
+        };
+        if (_options.init_call) callback.handler(this._dynamicConfig, null);
+    }
+    unsubscribe(id) {
+        delete this._callbacks[id];
+    }
+
+    get loaded() {
+        return this._initialized;
+    }
+
+    get dynamicConfig() {
+        if (!this._initialized) throw Error('Config not loaded');
+        return this._dynamicConfig;
+    }
+
+    get staticConfig() {
+        if (!this._initialized) throw Error('Config not loaded');
+        return this._staticConfig;
+    }
+
+    get d() {
+        return this.dynamicConfig;
+    }
+
+    get s() {
+        return this.staticConfig;
+    }
+    toString() {
+        return this._config.file;
+    }
+}
+
+
+module.exports = new Config();
